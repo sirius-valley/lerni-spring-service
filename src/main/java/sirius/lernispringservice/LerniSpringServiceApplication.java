ae8941751a771c469323ce0388a1748bed1a8a59
Benjamin Vivanco <benjamin.vivanco@ing.austral.edu.ar>
2024-01-15 11:36:54 -0300

    Initial commit

---
+++ src/main/java/sirius/lernispringservice/LerniSpringServiceApplication.java
@@ -0,0 +1,13 @@
+package sirius.lernispringservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LerniSpringServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LerniSpringServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sirius/lernispringservice/LerniSpringServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package sirius.lernispringservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LerniSpringServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
