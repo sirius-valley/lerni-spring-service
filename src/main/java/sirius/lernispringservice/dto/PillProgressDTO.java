db243d69f231115857221d48d96de9a420e78296
Benjamin Vivanco <benjamin.vivanco@ing.austral.edu.ar>
2024-01-19 19:46:04 -0300

    feat: added progress percentage

--- src/main/java/sirius/lernispringservice/dto/PillProgressDTO.java
+++ src/main/java/sirius/lernispringservice/dto/PillProgressDTO.java
@@ -5,15 +5,21 @@ import java.util.List;
 public class PillProgressDTO {
 
     private boolean completed;
+    private int progress;
     private List<PillNodeDTO> nodes;
 
-    public PillProgressDTO(boolean completed, List<PillNodeDTO> nodes){
+    public PillProgressDTO(boolean completed, int progress, List<PillNodeDTO> nodes){
         this.completed = completed;
+        this.progress = progress;
         this.nodes = nodes;
     }
 
     public boolean isCompleted() {
         return completed;
+    }
+
+    public int getProgress() {
+        return progress;
     }
 
     public List<PillNodeDTO> getNodes() {
--- src/main/java/sirius/lernispringservice/service/PillService.java
+++ src/main/java/sirius/lernispringservice/service/PillService.java
@@ -5,6 +5,7 @@ import form.Form;
 import form.node.FormNode;
 import form.node.FormNodeType;
 import form.result.FormTransitionResult;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
 import org.springframework.stereotype.Service;
 import sirius.lernispringservice.dto.*;
@@ -13,17 +14,15 @@ import visitor.Visitable;
 import visitor.Visitor;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-import java.util.Stack;
+import java.util.*;
 
 @Service
 public class PillService {
 
-    public PillProgressDTO getPillProgress (PillRequestDTO pillRequestDTO) throws ParseException, IOException {
+    public PillProgressDTO getPillProgress(PillRequestDTO pillRequestDTO) throws ParseException, IOException {
         //create form from json object
-        Form pillForm = FormFactory.createFormFromJsonObject(pillRequestDTO.getPillForm());
+        JSONObject pillJson = pillRequestDTO.getPillForm();
+        Form pillForm = FormFactory.createFormFromJsonObject(pillJson);
 
         //add user's answers to a stack
         Stack<PillAnswerDTO> stack = getAnswerStack(pillRequestDTO);
@@ -33,36 +32,39 @@ public class PillService {
         List<PillNodeDTO> pillProgress = new ArrayList<>();
 
         FormNode current;
-        do{
+        do {
             current = pillForm.getCurrent(visitedIds);
             FormTransitionResult result;
 
             pillProgress.add(getPillNodeDTO(current, stack.isEmpty() ? "" : stack.peek().getValue()));
-            if (current.getType() == FormNodeType.QUESTION){
-                if(stack.isEmpty() || !current.hasNext()) return new PillProgressDTO(!stack.isEmpty(), pillProgress);
+            if (current.getType() == FormNodeType.QUESTION) {
+                if (stack.isEmpty() || !current.hasNext())
+                    return new PillProgressDTO(!stack.isEmpty(), calculateProgress(pillJson, current.getId()), pillProgress);
                 PillAnswerDTO answer = stack.pop();
                 result = pillForm.getNext(answer.getValue(), visitedIds);
             } else {
-                if (!current.hasNext()) return new PillProgressDTO(true, pillProgress);
+                if (!current.hasNext())
+                    return new PillProgressDTO(true, calculateProgress(pillJson, current.getId()), pillProgress);
                 result = pillForm.getNext("answer", visitedIds);
             }
             visitedIds.add(result.getValue().getId());
         } while (current.hasNext());
 
-        return new PillProgressDTO(false, pillProgress);
+        return new PillProgressDTO(false, calculateProgress(pillJson, current.getId()), pillProgress);
     }
 
 
     public PillProgressDTO answerQuestion(AnswerRequestDTO answerRequestDTO) throws ParseException, IOException {
         //create form from json object
-        Form pillForm = FormFactory.createFormFromJsonObject(answerRequestDTO.getPillForm());
+        JSONObject pillJson = answerRequestDTO.getPillForm();
+        Form pillForm = FormFactory.createFormFromJsonObject(pillJson);
 
         List<String> visitedIds = new ArrayList<>();
         visitedIds.add(answerRequestDTO.getAnswer().getQuestionId());
         List<PillNodeDTO> pillProgress = new ArrayList<>();
 
-        if(!pillForm.hasNext(visitedIds)){
-            return new PillProgressDTO(true, pillProgress);
+        if (!pillForm.hasNext(visitedIds)) {
+            return new PillProgressDTO(true, calculateProgress(pillJson, pillForm.getCurrent(visitedIds).getId()), pillProgress);
         }
 
         FormTransitionResult answerResult = pillForm.getNext(answerRequestDTO.getAnswer().getValue(), visitedIds);
@@ -75,15 +77,16 @@ public class PillService {
             FormTransitionResult result;
             pillProgress.add(getPillNodeDTO(current, ""));
             if (current.getType() == FormNodeType.QUESTION) {
-                return new PillProgressDTO(false, pillProgress);
+                return new PillProgressDTO(false, calculateProgress(pillJson, current.getId()), pillProgress);
             } else {
-                if (!current.hasNext()) return new PillProgressDTO(true, pillProgress);
+                if (!current.hasNext())
+                    return new PillProgressDTO(true, calculateProgress(pillJson, current.getId()), pillProgress);
                 result = pillForm.getNext("answer", visitedIds);
             }
             visitedIds.add(result.getValue().getId());
         } while (current.hasNext());
 
-        return new PillProgressDTO(true, pillProgress);
+        return new PillProgressDTO(true, calculateProgress(pillJson, current.getId()), pillProgress);
     }
 
 
@@ -94,6 +97,13 @@ public class PillService {
         stack.addAll(answers);
         return stack;
     }
+
+    private int calculateProgress(JSONObject form, String currentId) {
+        List<LinkedHashMap<String, String>> elements = (List<LinkedHashMap<String, String>>) form.get("elements");
+        List<String> ids = elements.stream().map(map -> map.get("id")).toList();
+        return Math.round((float) ids.indexOf(currentId) / (ids.size() - 1) * 100);
+    }
+
 
     private NodeContentDTO getResponseNodeDTOWithVisitor(FormNode value) {
         Visitor<NodeContentDTO> visitor = new PillNodeResponseGeneratorVisitor();
